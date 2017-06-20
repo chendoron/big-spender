924f2458dc9a055f2ed63fd7a00fbb6f310122e0
Chen Doron <chendoron85@gmail.com>
2017-06-20 11:51:31 +0300

    Change parent components to dumb components and add the products into the store

--- app/index.jsx
+++ app/index.jsx
@@ -5,21 +5,12 @@ import PropTypes from 'prop-types';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/reducer.jsx';
-import FilterableProductTable from './components/FilterableProductTable/FilterableProductTable.jsx';
+import FilterableProductTable from './FilterableProductTable/FilterableProductTable.jsx';
 
 const user = {
   firstName: 'Chen',
   lastName: 'Doron',
 };
-
-const products = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
-];
 
 const store = createStore(reducer);
 
@@ -30,23 +21,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.user = props.user;
-    this.products = props.products;
   }
 
   static get propTypes() {
     return {
       user: PropTypes.object,
-      products: PropTypes.array.isRequired,
     };
   }
 
   render() {
     return (
       <Provider store={store}>
-        <FilterableProductTable products={this.products} />
+        <FilterableProductTable />
       </Provider>
     );
   }
 }
 
-ReactDOM.render(<App user={user} products={products} />, document.getElementById('root'));
+ReactDOM.render(<App user={user} />, document.getElementById('root'));
--- app/reducers/reducer.jsx
+++ app/reducers/reducer.jsx
@@ -1,11 +1,27 @@
 export default function reducer(state = {}, action = {}) {
   switch (action.type) {
     case 'FILTER_TEXT_CHANGE':
-      return { isStockOnly: state.isStockOnly, filterText: action.filterText };
+      return {
+        products: state.products,
+        filterText: action.filterText,
+        isStockOnly: state.isStockOnly,
+      };
     case 'STOCK_ONLY_CHANGE':
-      return { filterText: state.filterText, isStockOnly: action.isStockOnly };
+      return {
+        products: state.products,
+        filterText: state.filterText,
+        isStockOnly: action.isStockOnly,
+      };
     default:
       return {
+        products: [
+          { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+          { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+          { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+          { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+          { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+          { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+        ],
         filterText: '',
         isStockOnly: false,
       };
