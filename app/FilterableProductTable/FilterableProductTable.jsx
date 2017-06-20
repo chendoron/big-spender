5dacf33db600d5101bf1ed683c849709fdf4ade6
Chen Doron <chendoron85@gmail.com>
2017-06-20 11:58:48 +0300

    Refrain from usage of component extension when using a very-dumb component

--- app/FilterableProductTable/FilterableProductTable.jsx
+++ app/FilterableProductTable/FilterableProductTable.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Styles from './FilterableProductTable.scss';
 import SearchBar from './SearchBar/SearchBar.jsx';
 import ProductTable from './ProductTable/ProductTable.jsx';
 
-export default class FilterableProductTable extends React.Component {
-  static get propTypes() {
-    return {
-      user: PropTypes.object,
-    };
-  }
-
-  render() {
-    return (
-      <div className={Styles.filterableProductTable}>
-        <SearchBar />
-        <ProductTable />
-      </div>
-    );
-  }
+export default function FilterProductTable() {
+  return (
+    <div className={Styles.filterableProductTable}>
+      <SearchBar />
+      <ProductTable />
+    </div>
+  );
 }
